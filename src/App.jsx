72674587c2accf82f654f805ed2c62378cf810e0
dknott2602 <dknott2602@gmail.com>
2025-07-03 17:58:22 -0700

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Author from "./pages/Author";
 import ItemDetails from "./pages/ItemDetails";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import axios from 'axios'
+
+
 
 function App() {
   return (
--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,35 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Link, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick";
+
 
 const HotCollections = () => {
-const { id } = useParams();
-const [users, setUsers] = useState ([])
-const [loading, setLoading] = useState(true);
-const [data, setData] = useState ([])
-const [error, setError] = useState(null); 
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
 
-const fetchUsers = async () => {
-  try {
-    const { data } = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections');
-    setUsers(data); 
-    console.log(data); 
-  } catch (error) {
-    setError(error); 
-    console.error('Error fetching users:', error); 
-  } finally {
-    setLoading(false); 
-  }
-};
+  const fetchUsers = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+      );
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      setError(error);
+      console.error("Error fetching users:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-useEffect(() => {
-  fetchUsers(); 
-}, []);
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
-if (loading) return <div>Loading...</div>; 
-if (error) return <div>Error: {error.message}</div>; 
- 
-
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  
+    const settings = {
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1
+    }
 
   return (
     <section id="section-collections" className="no-bottom">
@@ -41,29 +51,42 @@ if (error) return <div>Error: {error.message}</div>;
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {data && data.map((item, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={item.nftImage} className="lazy img-fluid" alt="" /> 
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={item.authorImage} alt="" /> 
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{item.title}</h4> 
-                  </Link>
-                  <span>{item.tokenId}</span> 
+          <Slider {...settings}>
+
+          {data &&
+            data.map((item, index) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <Link to="/item-details">
+                      <img
+                        src={item.nftImage}
+                        className="lazy img-fluid"
+                        alt=""
+                        />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <Link to="/author">
+                      <img
+                        className="lazy pp-coll"
+                        src={item.authorImage}
+                        alt=""
+                        />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{item.title}</h4>
+                    </Link>
+                    <span>{item.tokenId}</span>
+                    <span>ERC-{item.code}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+            </Slider>
         </div>
       </div>
     </section>
