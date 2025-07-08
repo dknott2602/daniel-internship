f70b364d30ced72105dfcc9a788e8a8357f151da
dknott2602 <dknott2602@gmail.com>
2025-07-08 14:00:50 -0700

    skeleton loading

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -28,11 +28,6 @@ const HotCollections = () => {
   };
 
   useEffect(() => {
-    const image = new Image
-  image.src = item.nftImage
-  image.onload = () => {
-    setImg(image)
-  }
     fetchUsers();
   }, []);
 
@@ -45,111 +40,66 @@ const HotCollections = () => {
     slidesToScroll: 1,
   };
 
- 
-
   return (
     <section id="section-collections" className="no-bottom">
-
       <div className="container">
-
         <div className="row">
-
           <div className="col-lg-12">
-
             <div className="text-center">
-
               <h2>Hot Collections</h2>
 
               <div className="small-border bg-color-2"></div>
-
             </div>
-
           </div>
 
           <Slider {...settings}>
-
             {loading ? (
-
               <div>Loading...</div>
-
             ) : error ? (
-
               <div>Error: {error.message}</div>
-
             ) : (
-
-              data && data.map((item, index) => (
-
+              data &&
+              data.map((item, index) => (
                 <div className="" key={index}>
-
                   <div className="nft_coll">
-
                     <div className="nft_wrap">
-
                       <Link to="/item-details">
-
                         <img
-
                           src={item.nftImage}
-
                           className="lazy img-fluid"
-
                           alt={item.title || "NFT Collection"}
-
                         />
-
                       </Link>
-
                     </div>
 
                     <div className="nft_coll_pp">
-
                       <Link to="/author">
-
                         <img
-
                           className="lazy pp-coll"
-
                           src={item.authorImage}
-
-                          alt=${item.title} author}
-
+                          alt={`${item.title} author`}
                         />
-
                       </Link>
 
                       <i className="fa fa-check"></i>
-
                     </div>
 
                     <div className="nft_coll_info">
-
                       <Link to="/explore">
-
                         <h4>{item.title}</h4>
-
                       </Link>
 
                       <span>{item.tokenId}</span>
 
                       <span>ERC-{item.code}</span>
-
                     </div>
-
                   </div>
-
                 </div>
-
               ))
-
             )}
-
           </Slider>
-
         </div>
-
       </div>
-
     </section>
   );
 };
